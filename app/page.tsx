a3c058b1b128b912bb2a8ab804f06ffdd74a2588
N.I <iworksWeb@gmail.com>
2025-04-30 15:27:07 +0900

    add shadcn card to app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Link from "next/link";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
@@ -10,11 +11,20 @@ export default async function Home() {
 
   return (
     <main className="w-full max-w-3xl mx-auto my-16 px-2">
-      {lessons?.map((lesson) => (
-        <Link href={`/${lesson.id}/`} key={lesson.id}>
-          {lesson.title}
-        </Link>
-      ))}
+      <div className="flex flex-col gap-3">
+        {lessons?.map((lesson) => (
+          <Link href={`/${lesson.id}/`} key={lesson.id}>
+            <Card>
+              <CardHeader>
+                <CardTitle>{lesson.title}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>{lesson.description}</p>
+              </CardContent>
+            </Card>
+          </Link>
+        ))}
+      </div>
     </main>
   );
 }
