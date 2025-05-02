7f812f5e9e724cb058f9195965923ad26da41203
N.I <iworksWeb@gmail.com>
2025-05-02 16:16:10 +0900

    feat handleSignOut to components/auth/AuthClientButton.tsx

--- components/auth/AuthClientButton.tsx
+++ components/auth/AuthClientButton.tsx
@@ -3,16 +3,32 @@
 import { Session } from "@supabase/auth-helpers-nextjs";
 import { Button } from "../ui/button";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 
 const AuthClientButton = ({ session }: { session: Session | null }) => {
+  const router = useRouter();
   const supabase = createClientComponentClient();
+
   const handleSignIn = async () => {
     await supabase.auth.signInWithOAuth({
       provider: "github",
     });
   };
 
-  return <Button onClick={handleSignIn}>Login</Button>;
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
+  };
+
+  return (
+    <>
+      {session ? (
+        <Button onClick={handleSignOut}>LogOut</Button>
+      ) : (
+        <Button onClick={handleSignIn}>LogIn</Button>
+      )}
+    </>
+  );
 };
 
 export default AuthClientButton;
