4d7170fdf887cf1d67c3bdb5d54662cc57d11d69
N.I <iworksWeb@gmail.com>
2025-05-03 16:21:46 +0900

    fix public api validetion to app/api/create-stripe-customer/route.ts #2

--- app/api/create-stripe-customer/route.ts
+++ app/api/create-stripe-customer/route.ts
@@ -5,6 +5,7 @@ import { cookies } from "next/headers";
 /**
  *
  * @POST: http://localhost:3000/api/create-stripe-customer
+ * @param: API_ROUTE_SECRET: a36de50baa44c694df32c2d45db989066750fef0aa2b95fd5fee3b370344d93e 例
  * @body: {
  *  "id": "5623bb48-765c-4cfd-9b60-bf32a706aade", ※ profile.idを指定すること
  *  "email": "naobe@gmail.com"
@@ -12,6 +13,16 @@ import { cookies } from "next/headers";
  */
 export async function POST(req: NextRequest) {
   const supabase = createRouteHandlerClient({ cookies });
+  const query = req.nextUrl.searchParams.get("API_ROUTE_SECRET");
+  if (query !== process.env.API_ROUTE_SECRET) {
+    return NextResponse.json(
+      {
+        message: "Invalid API route secret",
+      },
+      { status: 401 }
+    );
+  }
+
   const data = await req.json();
   const { id, email } = data;
 
