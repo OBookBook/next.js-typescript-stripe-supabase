9ca7e4e47891e0b8f920e04b5c77edab6e5745d0
N.I <iworksWeb@gmail.com>
2025-05-03 16:16:31 +0900

    add profile model update stripe_customer column to app/api/create-stripe-customer/route.ts #2

--- app/api/create-stripe-customer/route.ts
+++ app/api/create-stripe-customer/route.ts
@@ -1,21 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import initStripe from "stripe";
-
+import { cookies } from "next/headers";
 /**
  *
  * @POST: http://localhost:3000/api/create-stripe-customer
  * @body: {
- *  "email": "test@gmail.com"
- * }
+ *  "id": "5623bb48-765c-4cfd-9b60-bf32a706aade", ※ profile.idを指定すること
+ *  "email": "naobe@gmail.com"
+}
  */
 export async function POST(req: NextRequest) {
+  const supabase = createRouteHandlerClient({ cookies });
   const data = await req.json();
-  const { email } = data;
+  const { id, email } = data;
 
   const stripe = new initStripe(process.env.STRIPE_SECRET_KEY!);
   const customer = await stripe.customers.create({
     email,
   });
+
+  await supabase
+    .from("profile")
+    .update({
+      stripe_customer: customer.id,
+    })
+    .eq("id", id);
 
   return NextResponse.json({
     message: `Stripe customer created: ${customer.id}`,
