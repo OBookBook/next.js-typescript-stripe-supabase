ff350f69c337c52a2b9e5cd3696a24c78741ab85
N.I <iworksWeb@gmail.com>
2025-05-07 23:58:47 +0900

    add await stripe?.redirectToCheckout({ sessionId: data.id }); #4

--- components/checkout/SubscriptionButton.tsx
+++ components/checkout/SubscriptionButton.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { loadStripe } from "@stripe/stripe-js";
 
 const SubscriptionButton = ({ planId }: { planId: string }) => {
   const processSubscription = async () => {
     const response = await fetch(
       `http://localhost:3000/api/subscription/${planId}`
     );
+
+    const data = await response.json();
+
+    const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY!);
+    await stripe?.redirectToCheckout({ sessionId: data.id });
   };
 
   return (
