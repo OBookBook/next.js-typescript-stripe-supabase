58aa235e75dfe8f080fc931e85d787b24ac99fa0
N.I <iworksWeb@gmail.com>
2025-05-09 00:28:04 +0900

    add await stripe.billingPortal.sessions #6

--- components/checkout/SubscriptionManagemntButton.tsx
+++ components/checkout/SubscriptionManagemntButton.tsx
@@ -1,11 +1,26 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { Button } from "../ui/button";
 
 const SubscriptionManagemntButton = () => {
+  const router = useRouter();
+
+  const loadPortal = async () => {
+    const response = await fetch("http://localhost:3000/api/portal");
+
+    if (!response.ok) {
+      console.error("Error fetching portal URL:", response.statusText);
+      return;
+    }
+
+    const data = await response.json();
+
+    router.push(data.url);
+  };
   return (
     <div>
-      <Button>サブスクリプション管理</Button>
+      <Button onClick={loadPortal}>サブスクリプション管理</Button>
     </div>
   );
 };
