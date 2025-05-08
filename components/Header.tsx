d2363af8d4e90ec32750c53e2dc58df2df2c80cc
N.I <iworksWeb@gmail.com>
2025-05-08 23:29:10 +0900

    add dashboard navi to components/Header.tsx #6

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import AuthServerButton from "./auth/AuthServerButton";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
-const Header = () => {
+const Header = async () => {
+  const supabase = createServerComponentClient({ cookies });
+  const { data: user } = await supabase.auth.getSession();
+
   return (
     <header className="flex py-4 px-6 border-b border-gray-200">
       <Link href="/">
         <Button variant="outline">Home</Button>
       </Link>
+      {user.session && (
+        <Link href="/dashboard" className="ml-4">
+          <Button variant="outline">dashboard</Button>
+        </Link>
+      )}
       <Link href="/pricing" className="ml-4">
         <Button variant="outline">price</Button>
       </Link>
