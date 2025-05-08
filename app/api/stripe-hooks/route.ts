5532886c4893120d55fd6619177380735a627ae2
N.I <iworksWeb@gmail.com>
2025-05-08 22:38:47 +0900

    feat stripe webhok post get event to app/api/stripe-hooks/route.ts #6

--- app/api/stripe-hooks/route.ts
+++ app/api/stripe-hooks/route.ts
@@ -1,7 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
+import initStripe from "stripe";
+import Stripe from "stripe";
 
 export async function POST(req: NextRequest) {
-  console.log("Stripe webhook received");
+  const stripe = new initStripe(process.env.STRIPE_SECRET_KEY!);
+  const signature = req.headers.get("stripe-signature");
+  const endpointSecret = process.env.STRIPE_SIGNING_SECRET!;
 
-  return NextResponse.json({ received: true });
+  const reqBuffer = Buffer.from(await req.arrayBuffer());
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      reqBuffer,
+      signature!,
+      endpointSecret
+    );
+
+    // console.log(event);
+    return NextResponse.json({ received: true });
+  } catch (error: any) {
+    return NextResponse.json(`Webhook Error: ${error.message}`, {
+      status: 401,
+    });
+  }
 }
