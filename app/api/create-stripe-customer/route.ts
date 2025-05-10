34968ebc38db47f67854fe932c01f36cad6689f5
N.I <iworksWeb@gmail.com>
2025-05-10 17:32:27 +0900

    fix build error to app/api/create-stripe-customer/route.ts #8

--- app/api/create-stripe-customer/route.ts
+++ app/api/create-stripe-customer/route.ts
@@ -2,15 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import initStripe from "stripe";
 import { cookies } from "next/headers";
-/**
- *
- * @POST: http://localhost:3000/api/create-stripe-customer
- * @param: API_ROUTE_SECRET: a36de50baa44c694df32c2d45db989066750fef0aa2b95fd5fee3b370344d93e 例
- * @body: {
- *  "id": "5623bb48-765c-4cfd-9b60-bf32a706aade", ※ profile.idを指定すること
- *  "email": "naobe@gmail.com"
-}
- */
+
 export async function POST(req: NextRequest) {
   const supabase = createRouteHandlerClient({ cookies });
   const query = req.nextUrl.searchParams.get("API_ROUTE_SECRET");
